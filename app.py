0b2179133df6c5197042a3482311825b509480f8
Gandalf2112121 <jeimerdinger2@gmail.com>
2024-12-13 07:41:26 -0300

    removep.p and requirements version

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
 from help import apology, login_required
 from dotenv import load_dotenv
 from cryptography.fernet import Fernet
--- p.py
+++
@@ -1,5 +0,0 @@
-from cryptography.fernet import Fernet
-
-# Generate a valid key
-key = Fernet.generate_key()
-print("Generated Key:", key.decode())
